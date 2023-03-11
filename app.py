92586b225c227b36218eea4817fd729ade47f9cc
jav <javier.quiroga.e@gmail.com>
2023-03-11 17:15:53 -0500

    Heroku stuff

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import Unauthorized
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "abc123"
